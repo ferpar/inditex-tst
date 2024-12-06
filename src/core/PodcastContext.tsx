644ba5d5976ababccee9c35c79efd0d91a9b9c13
Fer <ferp@protonmail.ch>
2024-12-06 16:24:52 +0100

    feat(app): add Context Provider, React Query, and CSS reset
    - Implemented a Context Provider for state management.
    - Installed React Query for data fetching and caching.
    - Added a CSS reset for consistent styling.
    - Removed unused CSS template to clean up the codebase.

---
+++ src/core/PodcastContext.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+interface Podcast {
+  id: string
+  title: string
+  description: string
+  imageUrl: string
+}
+
+interface PodcastContextType {
+  podcasts: Partial<Podcast>[]
+  setPodcasts: (podcasts: Podcast[]) => void
+  podCastDetail: Podcast
+  setPodCastDetail: (podCastDetail: Podcast) => void
+}
+
+export const initialPodcastDetail: Podcast = {
+  id: '',
+  title: '',
+  description: '',
+  imageUrl: '',
+}
+
+export const PodcastContext = React.createContext<PodcastContextType>({
+  podcasts: [],
+  setPodcasts: () => {},
+  podCastDetail: initialPodcastDetail,
+  setPodCastDetail: () => {},
+})
+
+export const PodcastProvider = ({ children }: React.PropsWithChildren) => {
+  const [podcasts, setPodcasts] = React.useState<Partial<Podcast>[]>([])
+  const [podCastDetail, setPodCastDetail] =
+    React.useState<Podcast>(initialPodcastDetail)
+  return (
+    <PodcastContext.Provider
+      value={{ podcasts, setPodcasts, podCastDetail, setPodCastDetail }}
+    >
+      {children}
+    </PodcastContext.Provider>
+  )
+}
